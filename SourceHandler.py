7ab8aefe98cbc7e92e2ab7f653b309b01cbf0e26
Ben DESKTOP <benevenshoshan@gmail.com>
2017-11-29 01:38:42 +0200

    SourceHandler update
    added the function which sorts images into .npy files which can be used as numpy arrays.
    added main.py for testing.
    see "rgb_gradient.png" for result after saving the values array, then loading it and showing the photo after it was saved in the .npy format.

--- SourceHandler.py
+++ SourceHandler.py
@@ -1,28 +1,28 @@
+"""
+This file is all about sorting files and working directories, filled with useful methods that I wrote.
+Each method has good documentation, and does a specific job.
+@author = Ben Even-Shoshan
+"""
+import imageio
 import os
 from scipy import misc
 import numpy as np
+import imageio
 
-#this method gets source folder as input and makes a filw for each image in it to train from.
+path = "C:\\Users\Ben\Documents\GitHub\Image_Interpreter\Oranges"
+
+
+#This method gets source folder as input and makes a filw for each image in it to train from.
 def sortFiles(path):
-    list_of_files = {}
-    for (dirpath, dirnames, filenames) in os.walk(path):
+    list_of_files = {} #Empty array which will be filled with the filenames
+    for (dirpath, dirnames, filenames) in os.walk(path): #Going in a loop through the directory.
         for filename in filenames:
             if filename.endswith('.jpg') or filename.endswith('.png') or filename.endswith('.jpeg'):
-                list_of_files[filename] = os.sep.join([dirpath, filename])
-    print (list_of_files)
-    for file in list_of_files:
-        arr = misc.imread(file, True)
-        np.savetxt(filename, arr)
+                list_of_files[filename] = os.sep.join([dirpath, filename]) #Adds files to array
+    n=0
+    for file in list_of_files: #Going through the files
+        arr = imageio.imread(list_of_files[filename]) #Creating an array to store the RGB values of each image
+        np.save(str(n), arr) #Saving each image's RGB values as text array files to go through later.
+        n = n+1
 
 
-
-
-
-
-
-
-
-
-arr = misc.imread('lena.png') # 640x480x3 array
-arr[20, 30] # 3-vector for a pixel
-arr[20, 30, 1] # green value for a pixel
